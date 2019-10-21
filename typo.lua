bcb4f2653d7f9b48299097aec9f1d06cde55945e
johndoe <johnd0e@mail.ua>
2019-10-21 14:23:38 +0300

    update Pandoc to 2.7.3
    typo.lua: fix titles, remove BOM
    
    dl_table: add fix for line numbers
    
    pandoc.exe: removed from repo (got too big github to host)
    
    also updated:
    - custom.html5
    - styles: pandoc.css, tango.css, pygments.css (extracted from generated standalone html)

--- typo.lua
+++ typo.lua
@@ -1,4 +1,6 @@
-﻿-- Links ----------------------------------------------------------------------
+--https://pandoc.org/lua-filters.html
+
+-- Links ----------------------------------------------------------------------
 function Link(el)
   if FORMAT:match"html" then
     el.target = el.target:gsub("%.md$",".html")
@@ -62,34 +64,14 @@ function CodeBlock(el)
 end
 
 -- pagetitle from header ------------------------------------------------------
-local function inlines_tostring (inlines) 
-  local strs = {} 
-  for i = 1, #inlines do 
-    strs[i] = tostring(inlines[i]) 
-  end 
-  return table.concat(strs) 
-end 
-
-for k, v in pairs(pandoc.Inline.constructor) do 
-  v.__tostring = function (inln) 
-    return ((inln.content and inlines_tostring(inln.content)) 
-        or (inln.caption and inlines_tostring(inln.caption)) 
-        or (inln.text and inln.text) 
-        or " ") 
-  end
-end
-
 local title
-function Header (el) 
+function Header(el) 
   if not title and el.level==1 then
-    title = inlines_tostring(el.content) 
+    title = pandoc.utils.stringify(el)
   end
 end 
 
 function Meta(meta)
   meta.pagetitle = title
   return meta
-end
-
-function Div(el)
 end
